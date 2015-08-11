bdee8770c14fa3c563255366ee4d4b913ebe3d3e
dijdev <danieljonesdev@gmail.com>
2015-08-10 20:49:02 -0400

    refactoring ish. adding json level generation

--- RoundTheBlockXDK/www/js/game.js
+++ RoundTheBlockXDK/www/js/game.js
@@ -1,5 +1,6 @@
 enchant();
 
+///Global Variables / Game Objects ///
 var gameWidth = 588;
 var gameHeight = 940;
 var game = new Core(gameWidth, gameHeight);
@@ -7,15 +8,17 @@ var background, ball, paddle, blocks;
 
 game.preload('./asset/textures/backgrounds/bgDark.jpg', './asset/arkPaddle.png', './asset/ball.png', './asset/blocks.png');
 
-var frame = 0, bgf = 0;
+//can likely remove due to refactor of background
+//var frame = 0, bgf = 0;
 var level = 0;
 
 game.onload = function(){
-		
+
+///Initiliaze game objects/functions///
 	gameObjectLoad();
 	ball = new Ball(25);
 	paddle = new Paddle(128, 30);
-	blocks  = new Array(18);
+	blocks  = new Array(0);
 	background = new Background();
 	for (var i = 0; i < blocks.length; i++)
 	{
@@ -30,7 +33,9 @@ game.onload = function(){
 		}
 
 	};
-	
+///---Initiliaze game objects/functions---///
+
+///Add Game Objects to scene///
 	game.rootScene.addChild(background);
 	game.rootScene.addChild(paddle);
 	game.rootScene.addChild(ball);
@@ -38,42 +43,65 @@ game.onload = function(){
 	{
 		game.rootScene.addChild(blocks[i]);
 	}
+///---Add Game Objects to scene---///	
 	
-	/*var block = new Sprite(65, 34);
-	block.image = game.assets['./asset/blocks.png'];
-	block.frame = 5;
-	game.rootScene.addChild(block);*/
+///Testing AJAX call for Level data///
+	var xmlhttp;
+	if (window.XMLHttpRequest)
+  	{// code for IE7+, Firefox, Chrome, Opera, Safari
+  		xmlhttp=new XMLHttpRequest();
+  	}
+	else
+ 	{// code for IE6, IE5
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange=function(){
+		if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    	{
+    		console.log(xmlhttp.responseText);
 
-	
+    		var testBlocks = new Array();
+    		var data = JSON.parse(xmlhttp.responseText);
+    		var rows = [];
+    		rows.push(data.rows.a);
+    		rows.push(data.rows.b);
+    		rows.push(data.rows.c);
+    		rows.push(data.rows.d);
+    		rows.push(data.rows.e);
+    		rows.push(data.rows.f);
+    		rows.push(data.rows.g);
+    		var rowa = data.rows.a;
+    		console.log(rows.length)
+    		for (var i = 0; i < rows.length; i++)
+    		{
+    			for (var j = 0; j < rows[i].length; j++)
+    			{
+    				console.log(rows[i][j]);
+    				console.log(j);
+	    			switch(rows[i][i])
+	    			{
+	    				case '0':
+	    				blocks.push(new Block(rows[i][i], 'a', i));
+	    				game.rootScene.addChild(blocks[i]);
+	    				break;
+	    				case '1':
+	    				blocks.push(new Block(rowb[i], 'b', i));
+	    				game.rootScene.addChild(blocks[i]);
+	    				break;
+	    			}
+    				
+    			}
+    			console.log(rows);
+    			console.log(rows[0]);
+    			console.log(rows[1]);
+    		}
+    	}
+	}
+	xmlhttp.open('GET', './levels/test00.json',true);
+	xmlhttp.send();
+///---Testing AJAX call for Level data---///
 
-	// var xmlhttp;
-	// if (window.XMLHttpRequest)
- //  	{// code for IE7+, Firefox, Chrome, Opera, Safari
- //  		xmlhttp=new XMLHttpRequest();
- //  	}
-	// else
- // 	{// code for IE6, IE5
-	// 	xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-	// }
-	// xmlhttp.onreadystatechange=function(){
-	// 	if (xmlhttp.readyState==4 && xmlhttp.status==200)
- //    	{
- //    		console.log(xmlhttp.responseText);
-
- //    		var testBlocks = new Array();
-    		
- //    		for (var i = 0; i < xmlhttp.responseText.length; i++)
- //    		{
-
- //    		}
- //    	}
-	// }
-	// xmlhttp.open('GET', './levels/test00',true);
-	// xmlhttp.send();
-
-	//game.rootScene.addChild(ball);
-	///---Ball---///
-
+///Game Inputs///
 	game.rootScene.addEventListener(Event.LEFT_BUTTON_DOWN, function(){
 		paddle.x -= 5;
 	});
@@ -87,6 +115,7 @@ game.onload = function(){
 	game.rootScene.addEventListener(Event.TOUCH_MOVE, function(evt){
 		paddle.x = evt.localX - 75;
 	});
-};
+///---Game Inputs---///
+};//End game.onload
 
 game.start();
--- RoundTheBlockXDK/www/js/gameObjects/block.js
+++ RoundTheBlockXDK/www/js/gameObjects/block.js
@@ -1,9 +1,33 @@
 Block = Class.create(Sprite, {
-		initialize: function(frm){
+		initialize: function(frm, row, cell){
 			enchant.Sprite.call(this, 65, 34);
 			this.image = game.assets['./asset/blocks.png'];
 			this.frame = frm;
-			this.y = 225;
+			switch (row){
+				case 'a':
+				this.y = 225;
+				break;
+				case 'b':
+				this.y = 259;
+				break;
+				case 'c':
+				break;
+				case 'd':
+				break;
+				case 'e':
+				break;
+				case 'f':
+				break;
+				case 'g':
+				break;
+			};
+			
+			this.x = this.width * cell;
+			
+			if (this.intersect(blocks))
+			{
+				console.log('something');
+			}
 			this.addEventListener(Event.ENTER_FRAME, this.die);
 		}
 	});
